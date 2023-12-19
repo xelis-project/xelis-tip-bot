2f651dcb56ef45f6424432adca22f7e8f805f380
Slixe <slixeprivate@gmail.com>
2023-12-19 14:24:14 +0100

    bot: use Poise framework for slash commands

--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,15 @@
 mod service;
 
-use anyhow::Result;
+use anyhow::{Result, Error};
 use clap::Parser;
-use serenity::{
-    Client,
-    all::{GatewayIntents, Message},
-    framework::{
-        standard::{
-            Configuration, macros::{group, command}, CommandResult
-        },
-        StandardFramework
-    },
-    client::Context, prelude::TypeMapKey,
-    builder::{CreateMessage, CreateEmbed, CreateEmbedFooter},
-    model::Colour,
-};
+use poise::{serenity_prelude::{GatewayIntents, ClientBuilder, CreateEmbed, CreateEmbedFooter, User, Colour}, CreateReply};
 use service::WalletService;
-use xelis_common::{network::Network, utils::format_xelis, config::COIN_DECIMALS};
+use xelis_common::{network::Network, utils::{format_xelis, from_xelis}, crypto::address::Address};
+
+type Context<'a> = poise::Context<'a, WalletService, Error>;
 
 const ICON: &str = "https://github.com/xelis-project/xelis-assets/raw/master/icons/png/square/green_background_black_logo.png?raw=true";
 const COLOR: u32 = 196559;
-
-#[group]
-#[commands(balance, deposit, withdraw, tip)]
-struct General;
-
-impl TypeMapKey for WalletService {
-    type Value = WalletService;
-}
 
 #[derive(Parser)]
 #[clap(version = "1.0.0", about = "XELIS Tip Bot")]
@@ -49,14 +31,25 @@ pub struct Config {
 #[tokio::main]
 async fn main() -> Result<()> {
     let config = Config::parse();
-    let framework = StandardFramework::new().group(&GENERAL_GROUP);
-    framework.configure(Configuration::new().prefix("/")); // set the bot's prefix to "~"
 
     let service = WalletService::new(config.wallet_name, config.password, config.network)?;
     let intents = GatewayIntents::non_privileged() | GatewayIntents::MESSAGE_CONTENT;
-    let mut client = Client::builder(config.token, intents)
+
+    let framework = poise::Framework::builder()
+        .options(poise::FrameworkOptions {
+            commands: vec![balance(), deposit(), withdraw(), tip()],
+            ..Default::default()
+        })
+        .setup(|ctx, _ready, framework| {
+            Box::pin(async move {
+                poise::builtins::register_globally(ctx, &framework.options().commands).await?;
+                Ok(service)
+            })
+        })
+        .build();
+
+    let mut client = ClientBuilder::new(config.token, intents)
         .framework(framework)
-        .type_map_insert::<WalletService>(service)
         .await?;
 
     // start listening for events by starting a single shard
@@ -67,130 +60,103 @@ async fn main() -> Result<()> {
     Ok(())
 }
 
-#[command]
-async fn balance(ctx: &Context, msg: &Message) -> CommandResult {
-    let channel = msg.channel_id.to_channel(&ctx.http).await?;
-    // Only allow this command in DM
-    if channel.private().is_none() {
-        return Ok(());
+/// Show your current balance
+#[poise::command(slash_command, broadcast_typing)]
+async fn balance(ctx: Context<'_>) -> Result<(), Error> {
+    // Retrieve balance for user
+    let service = ctx.data();
+    let balance = service.get_balance_for_user(ctx.author()).await;
+
+    let embed = CreateEmbed::default()
+        .title("Balance")
+        .field("Your balance is", format_xelis(balance), false)
+        .thumbnail(ICON)
+        .colour(COLOR);
+    let mut reply = CreateReply::default()
+        .embed(embed);
+
+    // Set reply to ephemeral if command was not used in DM
+    if ctx.channel_id().to_channel(ctx.http()).await?.private().is_none() {
+        reply = reply.ephemeral(true);
     }
 
-    let typing = msg.channel_id.start_typing(&ctx.http);
-    let data = ctx.data.read().await;
-    let service = data.get::<WalletService>().unwrap();
-    let balance = service.get_balance_for_user(msg.author.to_string()).await;
-    msg.channel_id.send_message(&ctx.http, CreateMessage::new()
-        .add_embed(CreateEmbed::default()
-            .title("Balance")
-            .field("Your balance is", format_xelis(balance), false)
-            .thumbnail(ICON)
-            .colour(COLOR)
-        )
-    ).await?;
-    typing.stop();
+    // Send reply
+    ctx.send(reply).await?;
 
     Ok(())
 }
 
-#[command]
-async fn deposit(ctx: &Context, msg: &Message) -> CommandResult {
-    let channel = msg.channel_id.to_channel(&ctx.http).await?;
-    // Only allow this command in DM
-    if channel.private().is_none() {
-        return Ok(());
+/// Show your deposit address
+#[poise::command(slash_command, broadcast_typing)]
+async fn deposit(ctx: Context<'_>) -> Result<(), Error> {
+    // Retrieve address for user
+    let service = ctx.data();
+    let address = service.get_address_for_user(ctx.author());
+
+    let embed = CreateEmbed::default()
+        .title("Deposit")
+        .field("Your deposit address is", address.to_string(), true)
+        .footer(CreateEmbedFooter::new("Please do not send any other coins than XELIS to this address"))
+        .thumbnail(ICON)
+        .colour(COLOR);
+
+    let mut reply = CreateReply::default()
+        .embed(embed);
+
+    // Set reply to ephemeral if command was not used in DM
+    if ctx.channel_id().to_channel(ctx.http()).await?.private().is_none() {
+        reply = reply.ephemeral(true);
     }
 
-    let typing = msg.channel_id.start_typing(&ctx.http);
-    let data = ctx.data.read().await;
-    let service = data.get::<WalletService>().unwrap();
-    let address = service.get_address_for_user(msg.author.to_string());
-
-    msg.channel_id.send_message(&ctx.http, CreateMessage::new()
-        .add_embed(CreateEmbed::default()
-            .title("Deposit")
-            .field("Your deposit address is", address.to_string(), true)
-            .footer(CreateEmbedFooter::new("Please do not send any other coins than XELIS to this address"))
-            .thumbnail(ICON)
-            .colour(COLOR)
-        )
-    ).await?;
-    typing.stop();
+    // Send reply
+    ctx.send(reply).await?;
 
     Ok(())
 }
 
+/// Withdraw from your balance
+#[poise::command(slash_command, broadcast_typing)]
+async fn withdraw(ctx: Context<'_>, address: String, amount: f64) -> Result<(), Error> {
+    let service = ctx.data();
+    let ephemeral = ctx.channel_id().to_channel(ctx.http()).await?.private().is_none();
 
-#[command]
-async fn withdraw(ctx: &Context, msg: &Message) -> CommandResult {
-    let channel = msg.channel_id.to_channel(&ctx.http).await?;
-    // Only allow this command in DM
-    if channel.private().is_none() {
-        return Ok(());
-    }
-
-    let typing = msg.channel_id.start_typing(&ctx.http);
-    let data = ctx.data.read().await;
-    let _ = data.get::<WalletService>().unwrap();
-    // TODO: implement withdraw
-
-    typing.stop();
-
-    Ok(())
-}
-
-#[command]
-async fn tip(ctx: &Context, msg: &Message) -> CommandResult {
-    let channel = msg.channel_id.to_channel(&ctx.http).await?;
-    // restrict this command in servers only
-    if channel.private().is_some() {
-        msg.channel_id.send_message(&ctx.http, CreateMessage::new()
-            .add_embed(CreateEmbed::default()
-                .title("Tip")
-                .description("This command is only available in servers")
-                .thumbnail(ICON)
-                .colour(Colour::RED)
-            )
-        ).await?;
-        return Ok(());
-    }
-
-    let typing = msg.channel_id.start_typing(&ctx.http);
-    let args: Vec<&str> = msg.content.split_whitespace().collect();
-
-    if args.len() != 3 {
-        msg.channel_id.send_message(&ctx.http, CreateMessage::new()
-            .add_embed(CreateEmbed::default()
-                .title("Tip")
-                .description("Please provide a user and an amount to tip")
-                .thumbnail(ICON)
-                .colour(Colour::RED)
-            )
-        ).await?;
-        return Ok(());
-    }
-
-    let user = match msg.mentions.first() {
-        Some(user) if !user.bot => user.id.to_string(),
-        _ => {
-            msg.channel_id.send_message(&ctx.http, CreateMessage::new()
-                .add_embed(CreateEmbed::default()
-                    .title("Tip")
-                    .description("Please provide a valid user to tip")
+    // Parse address in correct format
+    let to = match Address::from_string(&address) {
+        Ok(address) => address,
+        Err(e) => {
+            ctx.send(CreateReply::default().ephemeral(ephemeral).embed(
+                CreateEmbed::default()
+                    .title("Withdraw")
+                    .field("An error occured while withdrawing", e.to_string(), false)
                     .thumbnail(ICON)
                     .colour(Colour::RED)
                 )
             ).await?;
             return Ok(());
-        }
+        }    
     };
 
-    let amount = match from_xelis(args[2]) {
+    // Verify the address is in good network
+    if to.is_mainnet() != service.network().is_mainnet() {
+        ctx.send(CreateReply::default().ephemeral(ephemeral).embed(
+            CreateEmbed::default()
+                .title("Withdraw")
+                .field("An error occured while withdrawing", "Invalid network", false)
+                .thumbnail(ICON)
+                .colour(Colour::RED)
+            )
+        ).await?;
+        return Ok(());
+    }
+
+    // Parse amount in correct format
+    let amount = match from_xelis(amount.to_string()) {
         Some(amount) => amount,
         None => {
-            msg.channel_id.send_message(&ctx.http, CreateMessage::new()
-                .add_embed(CreateEmbed::default()
+            ctx.send(CreateReply::default().ephemeral(true).embed(
+                CreateEmbed::default()
                     .title("Tip")
-                    .description("Please provide a valid amount to tip")
+                    .field("An error occured while tipping", "Invalid amount", false)
                     .thumbnail(ICON)
                     .colour(Colour::RED)
                 )
@@ -199,23 +165,68 @@ async fn tip(ctx: &Context, msg: &Message) -> CommandResult {
         }
     };
 
-    let data = ctx.data.read().await;
-    let service = data.get::<WalletService>().unwrap();
-
-    match service.transfer(msg.author.id.to_string(), user.clone(), amount).await {
-        Ok(_) => {
-            msg.channel_id.send_message(&ctx.http, CreateMessage::new()
-                .add_embed(CreateEmbed::default()
-                    .title("Tip")
-                    .description(format!("You have tipped {} XEL to {}", format_xelis(amount), user))
+    match service.withdraw(ctx.author(), to, amount).await {
+        Ok(hash) => {
+            ctx.send(CreateReply::default().ephemeral(ephemeral).embed(
+                CreateEmbed::default()
+                    .title("Withdraw")
+                    .description(format!("You have withdrawn {} XEL", format_xelis(amount)))
+                    .field("Transaction", hash.to_string(), false)
                     .thumbnail(ICON)
                     .colour(COLOR)
                 )
             ).await?;
         },
         Err(e) => {
-            msg.channel_id.send_message(&ctx.http, CreateMessage::new()
-                .add_embed(CreateEmbed::default()
+            ctx.send(CreateReply::default().ephemeral(ephemeral).embed(
+                CreateEmbed::default()
+                    .title("Withdraw")
+                    .field("An error occured while withdrawing", e.to_string(), false)
+                    .thumbnail(ICON)
+                    .colour(Colour::RED)
+                )
+            ).await?;
+        }
+    };
+
+    Ok(())
+}
+
+/// Tip a user with XELIS
+#[poise::command(slash_command, broadcast_typing)]
+async fn tip(ctx: Context<'_>, #[description = "User to tip"] user: User, #[description = "Amount to tip"] amount: f64) -> Result<(), Error> {
+    let amount = match from_xelis(amount.to_string()) {
+        Some(amount) => amount,
+        None => {
+            ctx.send(CreateReply::default().ephemeral(true).embed(
+                CreateEmbed::default()
+                    .title("Tip")
+                    .field("An error occured while tipping", "Invalid amount", false)
+                    .thumbnail(ICON)
+                    .colour(Colour::RED)
+                )
+            ).await?;
+            return Ok(());
+        }
+    };
+
+    // Retrieve address for user
+    let service = ctx.data();
+
+    match service.transfer(ctx.author(), &user, amount).await {
+        Ok(_) => {
+            ctx.send(CreateReply::default().embed(
+                CreateEmbed::default()
+                    .title("Tip")
+                    .description(format!("{} have tipped {} XEL to {}", ctx.author(), format_xelis(amount), user))
+                    .thumbnail(ICON)
+                    .colour(COLOR)
+                )
+            ).await?;
+        },
+        Err(e) => {
+            ctx.send(CreateReply::default().ephemeral(true).embed(
+                CreateEmbed::default()
                     .title("Tip")
                     .field("An error occured while tipping", e.to_string(), false)
                     .thumbnail(ICON)
@@ -225,25 +236,5 @@ async fn tip(ctx: &Context, msg: &Message) -> CommandResult {
         }
     };
 
-    typing.stop();
-
     Ok(())
-}
-
-// Convert a XELIS amount from string to a u64
-pub fn from_xelis(value: impl Into<String>) -> Option<u64> {
-    let value = value.into();
-    let mut split = value.split('.');
-    let xelis: u64 = split.next()?.parse::<u64>().ok()?;
-    let decimals = split.next().unwrap_or("0");
-    if decimals.len() > COIN_DECIMALS as usize {
-        return None;
-    }
-
-    let mut decimals = decimals.parse::<u64>().ok()?;
-    while decimals > 0 && decimals % 10 == 0 {
-        decimals /= 10;
-    }
-
-    Some(xelis * 10u64.pow(COIN_DECIMALS as u32) + decimals)
 }
--- src/service.rs
+++ src/service.rs
@@ -1,12 +1,14 @@
 use std::{sync::Arc, path::Path};
 
 use anyhow::Result;
+use poise::serenity_prelude::User;
 use thiserror::Error;
 use xelis_common::{
+    crypto::hash::{Hash, Hashable},
     api::{DataValue, DataElement},
     network::Network,
     crypto::address::Address,
-    utils::format_xelis
+    utils::format_xelis, config::XELIS_ASSET
 };
 use xelis_wallet::{wallet::Wallet, storage::EncryptedStorage};
 
@@ -20,6 +22,8 @@ pub enum ServiceError {
     SelfTip,
     #[error("Not enough funds to transfer {} XEL", format_xelis(*.0))]
     NotEnoughFunds(u64),
+    #[error("Not enough funds to pay {} XEL of fee", format_xelis(*.0))]
+    NotEnoughFundsForFee(u64),
     #[error(transparent)]
     Any(#[from] anyhow::Error)
 }
@@ -43,8 +47,8 @@ impl WalletService {
         })
     }
 
-    fn get_balance_internal(&self, storage: &EncryptedStorage, user_id: String) -> u64 {
-        let balance = match storage.get_custom_data(BALANCES_TREE, &DataValue::String(user_id)) {
+    fn get_balance_internal(&self, storage: &EncryptedStorage, user: &User) -> u64 {
+        let balance = match storage.get_custom_data(BALANCES_TREE, &DataValue::U64(user.id.into())) {
             Ok(balance) => balance,
             Err(_) => return 0
         };
@@ -53,18 +57,18 @@ impl WalletService {
     }
 
     // Get the balance for a user based on its id
-    pub async fn get_balance_for_user(&self, user_id: String) -> u64 {
+    pub async fn get_balance_for_user(&self, user: &User) -> u64 {
         let storage = self.wallet.get_storage().read().await;
-        self.get_balance_internal(&storage, user_id)
+        self.get_balance_internal(&storage, user)
     }
 
     // Generate a deposit address for a user based on its id
-    pub fn get_address_for_user(&self, user_id: String) -> Address {
-        self.wallet.get_address_with(DataElement::Value(Some(DataValue::String(user_id))))
+    pub fn get_address_for_user(&self, user: &User) -> Address {
+        self.wallet.get_address_with(DataElement::Value(Some(DataValue::U64(user.id.into()))))
     }
 
     // Transfer XEL from one user to another
-    pub async fn transfer(&self, from: String, to: String, amount: u64) -> Result<(), ServiceError> {
+    pub async fn transfer(&self, from: &User, to: &User, amount: u64) -> Result<(), ServiceError> {
         if amount == 0 {
             return Err(ServiceError::Zero);
         }
@@ -74,17 +78,50 @@ impl WalletService {
         }
 
         let mut storage = self.wallet.get_storage().write().await;
-        let from_balance = self.get_balance_internal(&storage, from.clone());
+        let from_balance = self.get_balance_internal(&storage, &from);
         if amount > from_balance {
             return Err(ServiceError::NotEnoughFunds(amount));
         }
 
-        let to_balance = self.get_balance_internal(&storage, to.clone());
+        let to_balance = self.get_balance_internal(&storage, to);
 
         // Update balances
-        storage.set_custom_data(BALANCES_TREE, &DataValue::String(from), &DataElement::Value(Some(DataValue::U64(from_balance - amount))))?;
-        storage.set_custom_data(BALANCES_TREE, &DataValue::String(to), &DataElement::Value(Some(DataValue::U64(to_balance + amount))))?;
+        storage.set_custom_data(BALANCES_TREE, &DataValue::U64(from.id.into()), &DataElement::Value(Some(DataValue::U64(from_balance - amount))))?;
+        storage.set_custom_data(BALANCES_TREE, &DataValue::U64(to.id.into()), &DataElement::Value(Some(DataValue::U64(to_balance + amount))))?;
 
         Ok(())
+    }
+
+    // Withdraw XEL from the service to an address
+    pub async fn withdraw(&self, user: &User, to: Address, amount: u64) -> Result<Hash, ServiceError> {
+        if amount == 0 {
+            return Err(ServiceError::Zero);
+        }
+
+        let mut storage = self.wallet.get_storage().write().await;
+        let balance = self.get_balance_internal(&storage, &user);
+        if amount > balance {
+            return Err(ServiceError::NotEnoughFunds(amount));
+        }
+
+        let transaction = self.wallet.send_to(&storage, XELIS_ASSET, to, amount)?;
+        let fee = transaction.get_fee();
+        // Verify if he has enough with fees included
+        if fee + amount > balance {
+            return Err(ServiceError::NotEnoughFundsForFee(fee));
+        }
+
+        match self.wallet.submit_transaction(&transaction).await {
+            Ok(_) => {
+                // Update balance
+                storage.set_custom_data(BALANCES_TREE, &DataValue::U64(user.id.into()), &DataElement::Value(Some(DataValue::U64(balance - (fee + amount)))))?;
+                Ok(transaction.hash())
+            },
+            Err(e) => Err(ServiceError::Any(e.into()))
+        }
+    }
+
+    pub fn network(&self) -> &Network {
+        self.wallet.get_network()
     }
 }
